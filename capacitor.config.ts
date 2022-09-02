d83871b8cb25a39c031421478f8b8c996b647dbe
keviant16 <kevin-jean-charles@outlook.com>
2022-09-02 09:21:09 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'library-popular-web',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
