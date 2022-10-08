9769b18281d1a07d5c743530a800526ebd783e42
keviant16 <kevin-jean-charles@outlook.com>
2022-10-08 14:44:34 +0200

    update url

--- src/services/clients/Instance.tsx
+++ src/services/clients/Instance.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const jwtToken = localStorage.getItem('jwtToken')
 
 //const LOCALHOST = 'http://localhost:8080/api'
-const HEROKU = "https://library-popular-web-service.herokuapp.com"
+const HEROKU = "https://library-popular-web-service.herokuapp.com/api"
 
 export const client = axios.create({
     baseURL: HEROKU,
